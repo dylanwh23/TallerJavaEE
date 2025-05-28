f7a3b7509c7da462862c9882ec39afdb2dd445f6
diegopoBob <diegopozzi2@gmail.com>
2025-05-28 18:03:10 -0300

    v 1.3 todo transferencia hecho con mock taransferencia SOAP API

--- src/main/java/com/tallerjava/tallerjava/Transferencia/aplicacion/TransferenciaInterfase.java
+++ src/main/java/com/tallerjava/tallerjava/Transferencia/aplicacion/TransferenciaInterfase.java
@@ -2,11 +2,13 @@ package com.tallerjava.tallerjava.Transferencia.aplicacion;
 
 import com.tallerjava.tallerjava.Transferencia.dominio.Transferencia;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TransferenciaInterfase {
-    public boolean recibirNotificacionTransferenciaDesdeMedioPago (int monto,int idComercio);
+    public boolean recibirNotificacionTransferenciaDesdeMedioPago (int monto,int idComercio) ;
 
 
     public List<Transferencia> consultarDepositos(int idComercio, LocalDateTime fInicial, LocalDateTime fFinal);
