30f820308907eca137660f5ef7276c5ad5dedbbe
machichu <rmatias088@gmail.com>
2025-07-01 23:48:35 -0300

    Correccion de errores y eventos

--- src/main/java/com/tallerjava/tallerjava/Compra/aplicacion/CompraService.java
+++ src/main/java/com/tallerjava/tallerjava/Compra/aplicacion/CompraService.java
@@ -155,13 +155,11 @@ public class CompraService implements CompraInterface {
 
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("-------------- COMPRA RECHAZADA POR ERROR de red=? / prende el bancpo tarad--------------");
+            System.out.println("-------------- COMPRA RECHAZADA POR ERROR de red=? / prende el banco tarado esto es en compra--------------" + e);
             compra.setEstado(EnumEstadoCompra.DESAPROBADA);
             compraRepository.save(compra);
 
         }
-
-
 
         return compra;
     }
--- src/main/java/com/tallerjava/tallerjava/Transferencia/eventos/TransferenciaListener.java
+++ src/main/java/com/tallerjava/tallerjava/Transferencia/eventos/TransferenciaListener.java
@@ -5,10 +5,12 @@ import com.tallerjava.tallerjava.Transferencia.aplicacion.TransferenciaInterfase
 import com.tallerjava.tallerjava.Transferencia.aplicacion.TransferenciaService;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class TransferenciaListener {
 
+    @Inject
     TransferenciaInterfase transferenciaService;
     public void onTransferenciaSolicitada(@Observes TransferenciaSolicitadaEvent evento) {
         boolean ok = transferenciaService.recibirNotificacionTransferenciaDesdeMedioPago(evento.getMonto(), Math.toIntExact(evento.getIdComercio()));
